54ee69f1498c8742f4500da04103e529f2e358af
JimShaunUK <64266900+JimShaunUK@users.noreply.github.com>
2022-01-04 09:52:27 +0000

    using variables

---
+++ scripts.js
@@ -0,0 +1,14 @@
+//print to console
+console.log("Hello!");
+console.log(4+4);
+
+//variables
+var total = 14.0;
+console.log("£" + total); //concatentation
+console.log(`The total price is £${total}`); //interpolation
+
+
+
+
+
+
